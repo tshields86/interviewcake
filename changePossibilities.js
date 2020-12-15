c7f261fac69490c4fa22bc83f7ac9e3fcb8c90bd
Travis Shields <travis.shields@gmail.com>
2020-12-15 11:31:20 -0500

    Solve change possibilities

--- changePossibilities.js
+++ changePossibilities.js
@@ -1,15 +1,22 @@
 /* https://www.interviewcake.com/question/javascript/coin */
 
 /*
-
-
-
-
+Dynamic Programming
+Time: O(A * C)
+Space: O(A)
 */
 
-const changePossibilities = (amount, denominations) => {
-  let possibilities = 0;
+const changePossibilities = (amount, coins) => {
+  coins.sort((a, b) => a - b);
 
+  const dp = new Array(amount + 1).fill(0);
+  dp[0] = 1;
 
+  for (let coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] += dp[i - coin];
+    }
+  }
 
+  return dp[amount];
 };
